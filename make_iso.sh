13556e4a4093829d7b789ceeb0882343a009911f
Stanislau Vilchykau <donsgenii@gmail.com>
2020-07-19 12:32:19 +0300

    Build settings have been inproved.

--- make_iso.sh
+++ make_iso.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 rm -r isodir
+rm -r release
 
 mkdir -p isodir/boot/grub
+mkdir -p release
 
 cp build/kernel.bin isodir/boot/kernel.bin
 
 cp grub.cfg isodir/boot/grub/grub.cfg
-grub-mkrescue -o myos.iso isodir
+grub-mkrescue -o release/os.iso isodir
+
+rm -r isodir
