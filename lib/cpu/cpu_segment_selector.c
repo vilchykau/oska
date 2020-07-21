3c172dda868e5fec1a496160173879d4f0730c82
Stanislau Vilchykau <donsgenii@gmail.com>
2020-07-21 19:01:23 +0300

    Fuctions works with "cpu_segment_selector" have been added.

---
+++ lib/cpu/cpu_segment_selector.c
@@ -0,0 +1,12 @@
+#include "cpu/cpu_segment_selector.h"
+
+#include <stdint.h>
+
+uint32_t cpu_segment_selector_get_id(struct cpu_segment_selector* sel){
+    return (sel->value >> 3);
+}
+
+void cpu_segment_selector_set_id(struct cpu_segment_selector* sel, uint16_t id){
+    sel->value &= 0x7;
+    sel->value |= (id << 3);
+}
