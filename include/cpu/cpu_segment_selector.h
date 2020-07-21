ff6f92d1b0beba8d29d525e0e75edf8e77f6b757
Stanislau Vilchykau <donsgenii@gmail.com>
2020-07-21 18:53:11 +0300

    Header protect has been added. The struct "cpu_segment_selector_pack" has been added. It contains 3 "cpu_segment_selector"(s). The functions works with "cpu_segmet_selector" have been added.

--- include/cpu/cpu_segment_selector.h
+++ include/cpu/cpu_segment_selector.h
@@ -1,6 +1,22 @@
+#ifndef CPU_SEGMENT_SELECTOR_H
+#define CPU_SEGMENT_SELECTOR_H
+
+#include <stdint.h>
+
 struct cpu_segment_selector{
-    short int value;
+    uint16_t value;
 };
+
+#define CPU_SEL_CODE  0
+#define CPU_SEL_DATA  1
+#define CPU_SEL_STACK 2
+
+struct cpu_segment_selector_pack{
+    struct cpu_segment_selector sels[3];
+};
+
+uint32_t cpu_segment_selector_get_id(struct cpu_segment_selector* sel);
+void cpu_segment_selector_set_id(struct cpu_segment_selector* sel, uint16_t id);
 
 #define CPU_SEG_SEL_GETID(SEL) (SEL.value >> 3)
 #define CPU_SEG_SEL_SETID(SEL, ID) (SEL.value = (SEL.value & 0x7) | (ID << 3))
@@ -12,3 +28,5 @@ struct cpu_segment_selector{
 
 #define CPU_SEG_SEL_GET_PL(SEL) (SEL.value & 3)
 #define CPU_SEG_SEL_SET_PL(SEL, PL) (SEL.value = (SEL.value & 0xFFFC) | PL)
+
+#endif
