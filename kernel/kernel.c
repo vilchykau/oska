b17af382f8c27181f6a4c2e5495106288defb356
Stas Vilchikov <Donsgenii@gmail.com>
2020-07-30 08:14:21 +0300

    .

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -6,16 +6,16 @@
 #include "basic_terminal/basic_terminal.h"
 #include "cpu/cpu_segment_selector.h"
 
-void kernel_main(void) 
+void kernel_main(void)
 {
 	init_gdt();
 
 	struct cpu_segment_selector_pack system_pack;
 	kernel_alloc_program_pack(&system_pack);
-	
+
 
 	terminal_initialize();
 	terminal_writestring("Hello Woffrld!\n");
 
-	
+
 }
